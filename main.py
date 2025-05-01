7d74771b3f1b08ad868f53ca02b1ac3b7bd1fb80
drunkhuskey <sam.orent@gmail.com>
2025-05-01 14:27:06 -0400

    changed main to read text from frankenstien

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(filepath):
+    text = ""
+    
+    with open(filepath) as f:
+        text = f.read()
+        
+    return text
+
+def main():
+    path = "./books/frankenstein.txt"
+    print(get_book_text(path))
+    
+main()
